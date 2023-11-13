dfe7c4bd770eacd16ba92dfdeff99168050d3ea5
cs1ime <1602370940@qq.com>
2023-11-13 13:35:51 +0800

    update

--- modules/axruntime/src/hv/vmx/device_emu/myvirtdev.rs
+++ modules/axruntime/src/hv/vmx/device_emu/myvirtdev.rs
@@ -1,10 +1,18 @@
-
 use super::PortIoDevice;
 use hypercraft::{HyperResult, HyperError};
+use spin::Mutex;
+use core::{cell::{RefCell, RefMut}, borrow::BorrowMut};
+use crate::hv::vmx::device_emu::Arc;
 
 pub struct MyVirtDevice
 {
     port_base: u16,
+    inner: Arc<Mutex<MyVirtDeviceInner>>,
+}
+
+struct MyVirtDeviceInner
+{
+    latest_data : u32,
 }
 
 impl PortIoDevice for MyVirtDevice
@@ -14,18 +22,22 @@ impl PortIoDevice for MyVirtDevice
     }
 
     fn read(&self, _port: u16, _access_size: u8) -> HyperResult<u32> {
-        info!("[huaji] read!");
-        Ok(0xDEADBEEF)
+        let inner = self.inner.lock();
+        info!("[huaji] read! ret = {:#x}",inner.latest_data);
+        Ok(inner.latest_data)
     }
 
     fn write(&self, _port: u16, _access_size: u8, _value: u32) -> HyperResult {
-        info!("[huaji] write!");
+        info!("[huaji] write! val = {:#x}",_value);
+
+        let mut inner = self.inner.lock();
+        inner.latest_data = _value;
         Ok(()) // ignore write
     }
 }
 
 impl MyVirtDevice {
     pub fn new(port_base: u16) -> Self {
-        Self { port_base }
+        Self { port_base ,inner: Arc::new(Mutex::new(MyVirtDeviceInner{latest_data:0}))}
     }
 }
