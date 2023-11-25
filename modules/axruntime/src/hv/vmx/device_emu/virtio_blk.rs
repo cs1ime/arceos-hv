13a5536e75d627d7f1726f77574795eceddc2fac
cs1ime <1602370940@qq.com>
2023-11-25 22:10:30 +0800

    update

--- modules/axruntime/src/hv/vmx/device_emu/virtio_blk.rs
+++ modules/axruntime/src/hv/vmx/device_emu/virtio_blk.rs
@@ -1,7 +1,7 @@
 use hypercraft::{HyperResult, HyperError};
 use spin::Mutex;
 use core::{cell::{RefCell, RefMut}, borrow::BorrowMut};
-use crate::hv::vmx::device_emu::Arc;
+use crate::hv::vmx::device_emu::{Arc, virtio_mmio::VIRTIO_MMIO_MAGIC_VALUE};
 
 use super::{MmioDevice, virtio_mmio::{VirtMmioRegs, VirtioDeviceType}};
 
@@ -9,10 +9,6 @@ pub struct VirtBlk {
     mmio_start: usize,
     mmio_size: usize,
     inner: Arc<Mutex<VirtBlkInner>>
-}
-
-struct VirtBlkInner {
-    regs: VirtMmioRegs,
 }
 
 impl VirtBlk {
@@ -26,20 +22,10 @@ impl VirtBlk {
     }
 }
 
-impl MmioDevice for VirtBlk {
-    
-    fn mmio_range(&self) -> core::ops::Range<usize> {
-        self.mmio_start..(self.mmio_start+self.mmio_size)
-    }
-    fn read(&self,offset: usize) -> HyperResult<u32> {
-        info!("blk read offset = {:#x}",offset);
-        Ok(0)
-    }
-    fn write(&self,offset: usize,value: u32) -> HyperResult {
-        info!("blk write offset = {:#x} value = {:#x}",offset,value);
-        Ok(())
-    }
+struct VirtBlkInner {
+    regs: VirtMmioRegs,
 }
+
 
 impl VirtBlkInner {
     fn new() -> Self{
@@ -47,6 +33,31 @@ impl VirtBlkInner {
             regs: VirtMmioRegs::new(VirtioDeviceType::Block)
         }
     }
+
+    fn magic(&self) -> u32 {
+        self.regs.magic
+    }
+
 }
 
+impl MmioDevice for VirtBlk {
+    
+    fn mmio_range(&self) -> core::ops::Range<usize> {
+        self.mmio_start..(self.mmio_start+self.mmio_size)
+    }
+    fn read(&self,offset: usize) -> HyperResult<u32> {
+        info!("blk read offset = {:#x}",offset);
+
+        let inner = self.inner.lock();
+
+        match offset {
+            VIRTIO_MMIO_MAGIC_VALUE => Ok(inner.regs.magic),
+            _ => Ok(0)
+        }
+    }
+    fn write(&self,offset: usize,value: u32) -> HyperResult {
+        info!("blk write offset = {:#x} value = {:#x}",offset,value);
+        Ok(())
+    }
+}
 
--- modules/axruntime/src/hv/vmx/device_emu/virtio_mmio.rs
+++ modules/axruntime/src/hv/vmx/device_emu/virtio_mmio.rs
@@ -1,5 +1,32 @@
 #![no_std]
 
+pub const VIRTIO_F_VERSION_1: usize = 1 << 32;
+pub const VIRTIO_MMIO_MAGIC_VALUE: usize = 0x000;
+pub const VIRTIO_MMIO_VERSION: usize = 0x004;
+pub const VIRTIO_MMIO_DEVICE_ID: usize = 0x008;
+pub const VIRTIO_MMIO_VENDOR_ID: usize = 0x00c;
+pub const VIRTIO_MMIO_HOST_FEATURES: usize = 0x010;
+pub const VIRTIO_MMIO_HOST_FEATURES_SEL: usize = 0x014;
+pub const VIRTIO_MMIO_GUEST_FEATURES: usize = 0x020;
+pub const VIRTIO_MMIO_GUEST_FEATURES_SEL: usize = 0x024;
+pub const VIRTIO_MMIO_QUEUE_SEL: usize = 0x030;
+pub const VIRTIO_MMIO_QUEUE_NUM_MAX: usize = 0x034;
+pub const VIRTIO_MMIO_QUEUE_NUM: usize = 0x038;
+pub const VIRTIO_MMIO_QUEUE_READY: usize = 0x044;
+pub const VIRTIO_MMIO_QUEUE_NOTIFY: usize = 0x050;
+pub const VIRTIO_MMIO_INTERRUPT_STATUS: usize = 0x060;
+pub const VIRTIO_MMIO_INTERRUPT_ACK: usize = 0x064;
+pub const VIRTIO_MMIO_STATUS: usize = 0x070;
+pub const VIRTIO_MMIO_QUEUE_DESC_LOW: usize = 0x080;
+pub const VIRTIO_MMIO_QUEUE_DESC_HIGH: usize = 0x084;
+pub const VIRTIO_MMIO_QUEUE_AVAIL_LOW: usize = 0x090;
+pub const VIRTIO_MMIO_QUEUE_AVAIL_HIGH: usize = 0x094;
+pub const VIRTIO_MMIO_QUEUE_USED_LOW: usize = 0x0a0;
+pub const VIRTIO_MMIO_QUEUE_USED_HIGH: usize = 0x0a4;
+pub const VIRTIO_MMIO_CONFIG_GENERATION: usize = 0x0fc;
+pub const VIRTIO_MMIO_CONFIG: usize = 0x100;
+pub const VIRTIO_MMIO_REGS_END: usize = 0x200;
+
 pub enum VirtioDeviceType {
     None = 0,
     Net = 1,
@@ -7,11 +34,19 @@ pub enum VirtioDeviceType {
 }
 
 pub struct VirtMmioRegs {
-    magic: u32,
+    pub magic: u32,
     version: u32,
     device_id: u32,
     vendor_id: u32,
+    dev_feature: u32,
+    dev_feature_sel: u32,
+    drv_feature: u32,
+    drv_feature_sel: u32,
+    q_sel: u32,
     q_num_max: u32,
+    irt_stat: u32,
+    irt_ack: u32,
+    dev_stat: u32,
 }
 
 impl VirtMmioRegs {
@@ -20,9 +55,17 @@ impl VirtMmioRegs {
         Self {
             magic: 0x74726976,
             version: 0x2,
-            device_id: 0x8888,
-            vendor_id: id as u32,
+            vendor_id: 0x8888,
+            device_id: id as u32,
+            dev_feature: 0,
+            dev_feature_sel: 0,
+            drv_feature: 0,
+            drv_feature_sel: 0,
+            q_sel: 0,
             q_num_max: 0,
+            irt_stat: 0,
+            irt_ack: 0,
+            dev_stat: 0,
         }
     }
 }
